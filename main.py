81e02dd1d59ccac6e7b17cf9911c32bce3e9c882
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-07 16:49:06 -0400

    all4Motor

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def disconnect():
 
 def connected(*args):
     # Code here
-    for i in range(3000, 8000, 1000)
+    for i in range(3000, 8000, 1000):
         crazyflie.param.set_value("motors.motorPowerM1", str(i))
         crazyflie.param.set_value("motors.motorPowerM2", str(i))
         crazyflie.param.set_value("motors.motorPowerM3", str(i))
         crazyflie.param.set_value("motors.motorPowerM4", str(i))
-        for j in range(10000)
+        for j in range(10000):
             print(j)
     cutthewings()
     print("This is the end")
