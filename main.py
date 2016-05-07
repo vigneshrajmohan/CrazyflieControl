85f79615b00792158a277c820f916f296940fcd7
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-07 15:56:50 -0400

    Testing

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def disconnect():
 def connected(*args):
 
     # Code here
-    for i in range(11000):
-        print(i)
-        crazyflie.param.set_value("motors.motorPowerM1", str(i))
-        crazyflie.param.set_value("motors.motorPowerM2", str(i))
-        crazyflie.param.set_value("motors.motorPowerM3", str(i))
-        crazyflie.param.set_value("motors.motorPowerM4", str(i))
+    #for i in range(11000):
+        #print(i)
+        #crazyflie.param.set_value("motors.motorPowerM1", str(i))
+        #crazyflie.param.set_value("motors.motorPowerM2", str(i))
+        #crazyflie.param.set_value("motors.motorPowerM3", str(i))
+        #crazyflie.param.set_value("motors.motorPowerM4", str(i))
     cutthewings()
     print("This is the end")
 
