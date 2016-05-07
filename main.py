e59d26c756cf8ec747cc4ab4fcbfff9066e433b3
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-07 16:02:11 -0400

    Motor 4 Only

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def connected(*args):
 
     # Code here
     #for i in range(11000):
-        #print(i)
+        
         #crazyflie.param.set_value("motors.motorPowerM1", str(i))
         #crazyflie.param.set_value("motors.motorPowerM2", str(i))
         #crazyflie.param.set_value("motors.motorPowerM3", str(i))
