3b2581f8a1649e7833d972ed57a25307e8649655
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-07 17:04:52 -0400

    all4Motor

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def disconnect():
 
 def connected(*args):
     # Code here
-    for i in range(3000, 8000, 1000):
+    for i in range(3000, 8001, 1000):
         crazyflie.param.set_value("motors.motorPowerM1", str(i))
         crazyflie.param.set_value("motors.motorPowerM2", str(i))
         crazyflie.param.set_value("motors.motorPowerM3", str(i))
