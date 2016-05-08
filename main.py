26adcce44cf4f03b1f05f36693a375ae1d6bd9ef
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-08 15:23:13 -0400

    YEA SON

--- main.py
+++ main.py
@@ -136,8 +136,8 @@ class MotorRampExample:
 
     def _ramp_motors(self):
         thrust_mult = 1
-        thrust_step = 100
-        thrust = 35000
+        thrust_step = 200
+        thrust = 37000
         pitch = 0
         roll = 0
         yawrate = 0
