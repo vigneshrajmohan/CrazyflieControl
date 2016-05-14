90023bbf4e91b5039b35b27cd9a033ab13c41eb5
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-13 20:08:33 -0400

    May12-10:20 test

---
+++ hop/hop.rb
@@ -0,0 +1,20 @@
+
+require 'artoo'
+
+connection :crazyflie, :adaptor => :crazyflie
+device :drone, :driver => :crazyflie, :connection => :crazyflie, :interval => 0.1
+
+work do
+  drone.start
+  drone.take_off
+
+  after(1.5) {drone.hover(:start)}
+
+  after(3.seconds) {
+    drone.land
+  }
+
+  after(5.seconds) {
+    drone.stop
+  }
+end
