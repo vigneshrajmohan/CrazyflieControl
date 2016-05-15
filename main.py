f0d50b53e306714a3601ae8118ff3dba85d21276
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-05-15 19:27:19 -0400

    May 15 Test

--- main.py
+++ main.py
@@ -138,8 +138,8 @@ class MotorRampExample:
         thrust_mult = 1
         thrust_step = 1000
         # originally 200
-        thrust_initial = 35000
-        thrust = 30000
+        thrust_initial = 45000
+        thrust = 40000
         # originally 40000
         pitch = 0
         roll = 0
@@ -151,7 +151,7 @@ class MotorRampExample:
 
         self._cf.param.set_value("flightmode.althold","True")
         self._cf.commander.send_setpoint(0, 0, 0, thrust_initial)
-        time.sleep(2)
+        time.sleep(.5)
         for num in range(0,5):
             self._cf.param.set_value("flightmode.althold","True")
             self._cf.commander.send_setpoint(0, 0, 0, thrust)
